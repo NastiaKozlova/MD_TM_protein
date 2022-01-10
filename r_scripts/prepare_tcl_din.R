d1fbc6be8e4187cb05aa6b45659efc786d14d7e2
protdoc <KozlovaNastasiaSer@gmail.com>
2022-01-10 10:58:18 +0000

    optimase namd and namd energy stripts

--- r_scripts/prepare_tcl_din.R
+++ r_scripts/prepare_tcl_din.R
@@ -1,5 +1,7 @@
 #!/usr/bin/env R
 part_start = commandArgs(trailingOnly=TRUE)
+#install.packages("stringr")
+library(stringr)
 setwd(part_start)
 v_parta<-list.files('MD')
 v_part<-paste0(part_start,'MD/',v_parta)
@@ -9,6 +11,7 @@ if (!dir.exists(paste0(part_start,'MD_analysis/tcl/'))){dir.create(paste0(part_s
 if (!dir.exists(paste0(part_start,'MD_analysis/hbonds/'))){dir.create(paste0(part_start,'MD_analysis/hbonds/'))}
 i<-1
 q<-1
+j<-1
 for (j in 1:length(v_parta)) {
   part<-paste0(part_start,'MD/',v_parta[j])
   parta<-paste0(v_parta)
@@ -23,6 +26,25 @@ for (j in 1:length(v_parta)) {
   if (!dir.exists(paste0(part,'/din/SASA'))){dir.create(paste0(part,'/din/SASA'))}
   if (!dir.exists(paste0(part,'/din/Energy'))){dir.create(paste0(part,'/din/Energy'))}
   if (!dir.exists(paste0(part,'/din/hbonds'))){dir.create(paste0(part,'/din/hbonds'))}
+  
+  df_conf<-read.table(file = paste0(part,"/namd/step7_production.inp"),sep="?")
+  v_paraneters<-c()
+  for (i in 1:nrow(df_conf)) {
+    if(length(df_conf[i,1])>0&(grepl( df_conf[i,1], pattern = "parameters", fixed = TRUE))){
+      v_paraneters<-c(v_paraneters,df_conf[i,1])
+    }
+  }
+  str_view(v_paraneters, "toppar/")
+  a<-  str_locate(v_paraneters, "toppar/")
+  i<-1
+  for (i in 1:length(v_paraneters)) {
+    seq<-strsplit(v_paraneters[i],split = "",fixed = T)[[1]]
+    v_paraneters[i]<-paste0(seq[a[i,1]:length(seq)],collapse = "")
+  }
+  v_paraneters<-paste0("-par namd/",v_paraneters)
+  v_paraneters<-paste(v_paraneters,collapse = " ")
+  
+  
   df_tcl<-data.frame(matrix(nrow = 1,ncol = 1))
   df_tcl[1,1]<-paste('cd', part,"\npackage require namdenergy")
   df_tcl[2,1]<-paste0('mol new {namd/step5_input.psf} type {psf}')
@@ -30,9 +52,10 @@ for (j in 1:length(v_parta)) {
   df_tcl[4,1]<-paste0('set sel0 [atomselect top "water"]')
   df_tcl[5,1]<-paste0('set sel1 [atomselect top "lipid or resname POPG"]')
   df_tcl[6,1]<-paste0('set sel2 [atomselect top "protein"]')
-  df_tcl[7,1]<-paste0('namdenergy -sel $sel2  $sel0 -vdw -elec -nonb -cutoff 12 -skip 0 -ofile din/Energy/protein_water_energy_',8,'.txt -switch 10 -exe ',namd_exe,' -par namd/toppar/par_all36_cgenff.prm -par namd/toppar/toppar_all36_na_rna_modified.str -par namd/toppar/toppar_all36_label_fluorophore.str -par namd/toppar/par_all36m_prot.prm -par namd/toppar/par_all36_carb.prm -par namd/toppar/toppar_all36_carb_imlab.str -par namd/toppar/toppar_all36_synthetic_polymer.str -par namd/toppar/toppar_all36_label_spin.str -par namd/toppar/par_interface.prm -par namd/toppar/toppar_all36_prot_c36m_d_aminoacids.str -par namd/toppar/par_all36_na.prm -par namd/toppar/toppar_all36_prot_modify_res.str -par namd/toppar/toppar_all36_lipid_cholesterol.str -par namd/toppar/toppar_all36_lipid_sphingo.str -par namd/toppar/par_all36_lipid.prm -par namd/toppar/toppar_all36_carb_glycolipid.str -par namd/toppar/toppar_all36_lipid_lps.str -par namd/toppar/toppar_all36_prot_retinol.str -par namd/toppar/toppar_all36_carb_glycopeptide.str -par namd/toppar/toppar_all36_prot_na_combined.str -par namd/toppar/toppar_all36_prot_heme.str -par namd/toppar/toppar_all36_na_nad_ppi.str -par namd/toppar/toppar_all36_lipid_ether.str -par namd/toppar/toppar_water_ions.str -par namd/toppar/toppar_all36_prot_arg0.str -par namd/toppar/toppar_ions_won.str -par namd/toppar/toppar_all36_lipid_bacterial.str -par namd/toppar/toppar_all36_nano_lig.str -par namd/toppar/toppar_all36_lipid_detergent.str -par namd/toppar/toppar_all36_prot_fluoro_alkanes.str -par namd/toppar/toppar_all36_lipid_prot.str -par namd/toppar/toppar_all36_lipid_miscellaneous.str -par namd/toppar/toppar_all36_lipid_hmmm.str -par namd/toppar/toppar_all36_lipid_yeast.str -par namd/toppar/toppar_dum_noble_gases.str')
-  df_tcl[8,1]<-paste0('namdenergy -sel $sel2  $sel1 -vdw -elec -nonb -cutoff 12 -skip 0 -ofile din/Energy/protein_lipid_energy_',8,'.txt -switch 10 -exe ',namd_exe,' -par namd/toppar/par_all36_cgenff.prm -par namd/toppar/toppar_all36_na_rna_modified.str -par namd/toppar/toppar_all36_label_fluorophore.str -par namd/toppar/par_all36m_prot.prm -par namd/toppar/par_all36_carb.prm -par namd/toppar/toppar_all36_carb_imlab.str -par namd/toppar/toppar_all36_synthetic_polymer.str -par namd/toppar/toppar_all36_label_spin.str -par namd/toppar/par_interface.prm -par namd/toppar/toppar_all36_prot_c36m_d_aminoacids.str -par namd/toppar/par_all36_na.prm -par namd/toppar/toppar_all36_prot_modify_res.str -par namd/toppar/toppar_all36_lipid_cholesterol.str -par namd/toppar/toppar_all36_lipid_sphingo.str -par namd/toppar/par_all36_lipid.prm -par namd/toppar/toppar_all36_carb_glycolipid.str -par namd/toppar/toppar_all36_lipid_lps.str -par namd/toppar/toppar_all36_prot_retinol.str -par namd/toppar/toppar_all36_carb_glycopeptide.str -par namd/toppar/toppar_all36_prot_na_combined.str -par namd/toppar/toppar_all36_prot_heme.str -par namd/toppar/toppar_all36_na_nad_ppi.str -par namd/toppar/toppar_all36_lipid_ether.str -par namd/toppar/toppar_water_ions.str -par namd/toppar/toppar_all36_prot_arg0.str -par namd/toppar/toppar_ions_won.str -par namd/toppar/toppar_all36_lipid_bacterial.str -par namd/toppar/toppar_all36_nano_lig.str -par namd/toppar/toppar_all36_lipid_detergent.str -par namd/toppar/toppar_all36_prot_fluoro_alkanes.str -par namd/toppar/toppar_all36_lipid_prot.str -par namd/toppar/toppar_all36_lipid_miscellaneous.str -par namd/toppar/toppar_all36_lipid_hmmm.str -par namd/toppar/toppar_all36_lipid_yeast.str -par namd/toppar/toppar_dum_noble_gases.str')
-  df_tcl[9,1]<-paste0('namdenergy -sel $sel2        -vdw -elec -nonb -cutoff 12 -skip 0 -ofile din/Energy/protein_',8,             '.txt -switch 10 -exe ',namd_exe,' -par namd/toppar/par_all36_cgenff.prm -par namd/toppar/toppar_all36_na_rna_modified.str -par namd/toppar/toppar_all36_label_fluorophore.str -par namd/toppar/par_all36m_prot.prm -par namd/toppar/par_all36_carb.prm -par namd/toppar/toppar_all36_carb_imlab.str -par namd/toppar/toppar_all36_synthetic_polymer.str -par namd/toppar/toppar_all36_label_spin.str -par namd/toppar/par_interface.prm -par namd/toppar/toppar_all36_prot_c36m_d_aminoacids.str -par namd/toppar/par_all36_na.prm -par namd/toppar/toppar_all36_prot_modify_res.str -par namd/toppar/toppar_all36_lipid_cholesterol.str -par namd/toppar/toppar_all36_lipid_sphingo.str -par namd/toppar/par_all36_lipid.prm -par namd/toppar/toppar_all36_carb_glycolipid.str -par namd/toppar/toppar_all36_lipid_lps.str -par namd/toppar/toppar_all36_prot_retinol.str -par namd/toppar/toppar_all36_carb_glycopeptide.str -par namd/toppar/toppar_all36_prot_na_combined.str -par namd/toppar/toppar_all36_prot_heme.str -par namd/toppar/toppar_all36_na_nad_ppi.str -par namd/toppar/toppar_all36_lipid_ether.str -par namd/toppar/toppar_water_ions.str -par namd/toppar/toppar_all36_prot_arg0.str -par namd/toppar/toppar_ions_won.str -par namd/toppar/toppar_all36_lipid_bacterial.str -par namd/toppar/toppar_all36_nano_lig.str -par namd/toppar/toppar_all36_lipid_detergent.str -par namd/toppar/toppar_all36_prot_fluoro_alkanes.str -par namd/toppar/toppar_all36_lipid_prot.str -par namd/toppar/toppar_all36_lipid_miscellaneous.str -par namd/toppar/toppar_all36_lipid_hmmm.str -par namd/toppar/toppar_all36_lipid_yeast.str -par namd/toppar/toppar_dum_noble_gases.str')
+  
+  df_tcl[7,1]<-paste0('namdenergy -sel $sel2  $sel0 -vdw -elec -nonb -cutoff 12 -skip 0 -ofile din/Energy/protein_water_energy_',8,'.txt -switch 10 -exe ',namd_exe,' ',v_paraneters)
+  df_tcl[8,1]<-paste0('namdenergy -sel $sel2  $sel1 -vdw -elec -nonb -cutoff 12 -skip 0 -ofile din/Energy/protein_lipid_energy_',8,'.txt -switch 10 -exe ',namd_exe,' ',v_paraneters)
+  df_tcl[9,1]<-paste0('namdenergy -sel $sel2        -vdw -elec -nonb -cutoff 12 -skip 0 -ofile din/Energy/protein_',8,             '.txt -switch 10 -exe ',namd_exe,' ',v_paraneters)
   df_tcl[10,1]<-'mol delete all\n\n\n exit now'
   write.table(df_tcl,file =paste0(part_start,'MD_analysis/tcl/',parta[j],'_Energy_',8,'.tcl'),sep = '\n', quote = F,na = '' ,row.names = F,col.names = F)
   
