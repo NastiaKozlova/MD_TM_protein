78979d9efb9592bb4b40d98861644973e23a98da
NastiaKozlova <Kozlovaas.93@gmail.com>
2022-05-17 10:56:46 +0100

    fix mistakes

--- r_scripts/docking/merge_interactions.R
+++ r_scripts/docking/merge_interactions.R
@@ -17,20 +17,22 @@ if (dir.exists(paste0("interaction_serf/"))) {system(command = paste0("rm -r ",p
 
 if (!dir.exists(paste0("interaction_serf/"))) { dir.create(paste0("interaction_serf/"))}
 i<-1
-j<-1
+j<-3
 p<-1
 v_structure<-unique(df_all$name.x)
 for (j in 1:length(v_structure)) {
-  df_complex<-df_all%>%filter(name.x==v_structure[i])
+  df_complex<-df_all%>%filter(name.x==v_structure[j])
   pdb<-read.pdb(paste0(part_name,"receptor_start/",df_all$receptor[j],".pdb"))
+
   df_pdb<-pdb$atom
   df_pdb<-df_pdb%>%filter(elety=="CA")
   df_pdb<-df_pdb%>%mutate(number_interactions=0)
   df_pdb<-df_pdb%>%mutate(tested_structure=0)
   df_pdb<-df_pdb%>%mutate(total_structure=nrow(df_complex))
+  test<-nrow(df_pdb)
   for (p in 1:nrow(df_complex)) {
-    if(file.exists(paste0("interaction/",df_all$receptor_ligand[p],"/",df_complex$new_number[p],".csv"))){
-      df_protein<-read.csv(paste0("interaction/",df_all$receptor_ligand[p],"/",df_complex$new_number[p],".csv"),
+    if(file.exists(paste0("interaction/",df_complex$receptor_ligand[p],"/",df_complex$new_number[p],".csv"))){
+      df_protein<-read.csv(paste0("interaction/",df_complex$receptor_ligand[p],"/",df_complex$new_number[p],".csv"),
                            stringsAsFactors = F) 
       df_pdb$number_interactions[df_pdb$resno%in%df_protein$resid]<-df_pdb$number_interactions[df_pdb$resno%in%df_protein$resid]+1
       df_pdb$tested_structure<-df_pdb$tested_structure+1
@@ -38,7 +40,9 @@ for (j in 1:length(v_structure)) {
     }
   }
   df_pdb<-df_pdb%>%filter(tested_structure==total_structure)
-  if(nrow(df_pdb)>0){
+  if(nrow(df_pdb)==test){
+    df_pdb<-df_pdb%>%select(resno,resid,x,y,z,number_interactions,tested_structure,total_structure)
+    df_pdb<-df_pdb%>%mutate(persent_interactions=number_interactions/total_structure*100)
     write.csv(df_pdb,
               paste0("interaction_serf/",v_structure[j],".csv"),row.names = F)
   }
