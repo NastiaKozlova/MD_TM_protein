aacd7bdc6cb2fe93284bad6d2d221a0055fcb2c6
NastiaKozlova <Kozlovaas.93@gmail.com>
2022-06-20 13:38:37 +0100

    fix script name

--- r_scripts/docking_active_center.R
+++ r_scripts/docking_active_center.R
@@ -71,5 +71,5 @@ system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/docking/merg
 
 system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/docking/merge_interactions_center.R ",part_name),ignore.stdout=T,wait = T)
 
-system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/docking/complex_structure_center.R ",part_analysis),ignore.stdout=T,wait = T)
-system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/docking/atom_interactions_center.R ",part_analysis),ignore.stdout=T,wait = T)
+system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/docking/complex_structure_center.R ",part_name),ignore.stdout=T,wait = T)
+system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/docking/atom_interactions_center.R ",part_name),ignore.stdout=T,wait = T)
