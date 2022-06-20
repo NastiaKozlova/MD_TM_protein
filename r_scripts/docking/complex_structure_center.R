aff79f5e19a38a3491bb681382cf332a0d093907
NastiaKozlova <Kozlovaas.93@gmail.com>
2022-06-20 12:49:43 +0100

    add file rewriting

--- r_scripts/docking/complex_structure_center.R
+++ r_scripts/docking/complex_structure_center.R
@@ -6,11 +6,10 @@ library(ggplot2)
 paste0(part_analysis)
 
 df_merge<-read.csv(paste0(part_analysis,"din/","df_merge_structure_log_center.csv"),stringsAsFactors = F)
-#df_merge<-df_merge%>%mutate(name=paste0(ligand,"_",center,"_",models.x))
-#df_merge<-semi_join(df_merge,df_all)
 df_merge<-df_merge%>%select(name.x,receptor,ligand,size_of_group)
 df_merge<-unique(df_merge)
-i<-1
+if(dir.exists(paste0(part_analysis,"din/complex_structure_center"))) {system(command = paste0("rm -r ",part_analysis,"din/complex_structure_center"),ignore.stdout=T,wait = T)}
+
 if(!dir.exists(paste0(part_analysis,"din/complex_structure_center"))){dir.create(paste0(part_analysis,"din/complex_structure_center"))}
 for (i in 1:nrow(df_merge)) {
   receptor_name<-paste0(part_analysis,"receptor_start/",df_merge$receptor[i],".pdb")
