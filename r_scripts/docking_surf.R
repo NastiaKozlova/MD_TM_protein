e610a701b9d947837a5fa49a20c8efb68c317c28
NastiaKozlova <Kozlovaas.93@gmail.com>
2023-02-01 18:31:39 +0000

    fix bugs

--- r_scripts/docking_surf.R
+++ r_scripts/docking_surf.R
@@ -8,13 +8,10 @@ part_name<-paste0(part_start,"MD_analysis/docking/docking_first/")
 
 system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/docking/docking_group_structure.R ",part_name,",1"),ignore.stdout=T,wait = T)
 
-
 system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/docking/RMSD_merge_docking_parts.R ",part_name),ignore.stdout=T,wait = T)
 system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/docking/calibration_docking_merge_structure.R ",part_name),ignore.stdout=T,wait = T)
-
 system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/docking/merge_docking_parts.R ",part_name,",1"),ignore.stdout=T,wait = T)
-
 system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/docking/merge_interactions.R ",part_name),ignore.stdout=T,wait = T)
 
 system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/docking/complex_structure_surf.R ",part_name),ignore.stdout=T,wait = T)
-system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/docking/atom_interactions.R ",part_name),ignore.stdout=T,wait = T)
+system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/docking/atom_interactions_surf.R ",part_name),ignore.stdout=T,wait = T)
