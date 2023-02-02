21eed14b1985e28ef62ef6cfa5c83049f4d4fb27
NastiaKozlova <Kozlovaas.93@gmail.com>
2023-02-02 14:52:28 +0000

    separate docking scripts

--- r_scripts/docking_surf.R
+++ r_scripts/docking_surf.R
@@ -6,7 +6,7 @@ setwd(part_start)
 
 part_name<-paste0(part_start,"MD_analysis/docking/docking_first/")
 
-system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/docking/docking_group_structure.R ",part_name,",1"),ignore.stdout=T,wait = T)
+#system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/docking/docking_group_structure.R ",part_name,",1"),ignore.stdout=T,wait = T)
 
 system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/docking/RMSD_merge_docking_parts.R ",part_name),ignore.stdout=T,wait = T)
 system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/docking/calibration_docking_merge_structure.R ",part_name),ignore.stdout=T,wait = T)
