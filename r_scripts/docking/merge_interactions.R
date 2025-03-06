fe4a1cf7809e566aa37930e113b21f175adf161b
NastiaKozlova <kozlovaas.93gmail.com>
2025-03-06 16:12:08 -0600

    fix script

--- r_scripts/docking/merge_interactions.R
+++ r_scripts/docking/merge_interactions.R
@@ -2,14 +2,14 @@ part_name <- commandArgs(trailingOnly=TRUE)
 library(bio3d)
 library(dplyr)
 library(ggplot2)
-v_rmsd<-4
+#v_rmsd<-4
 
 setwd(part_name)
 setwd("din")
 
 
 df_all<-read.csv(paste0("df_merge_structure_log.csv"),stringsAsFactors = F)
-df_all<-df_all%>%mutate(receptor_ligand=paste0(receptor,"_",ligand,"_",center))
+df_all<-df_all%>%mutate(receptor_ligand=paste0(receptor,"_",ligand,"_",center.x))
 
 #if (dir.exists(paste0("interaction/"))) { system(command = paste0("rm -r ",part_name,"din/interaction/"))}
 #if (dir.exists(paste0("interaction_TEMP/"))) {system(command = paste0("rm -r ",part_name,"din/interaction_TEMP/"))}
@@ -17,36 +17,46 @@ if (dir.exists(paste0("interaction_serf/"))) {system(command = paste0("rm -r ",p
 
 if (!dir.exists(paste0("interaction_serf/"))) { dir.create(paste0("interaction_serf/"))}
 i<-1
-j<-3
+j<-1
 p<-1
 v_structure<-unique(df_all$name.x)
 for (j in 1:length(v_structure)) {
   df_complex<-df_all%>%filter(name.x==v_structure[j])
   pdb<-read.pdb(paste0(part_name,"receptor_start/",df_all$receptor[j],".pdb"))
-
+  
   df_pdb<-pdb$atom
   df_pdb<-df_pdb%>%filter(elety=="CA")
   df_pdb<-df_pdb%>%mutate(number_interactions=0)
   df_pdb<-df_pdb%>%mutate(tested_structure=0)
   df_pdb<-df_pdb%>%mutate(total_structure=nrow(df_complex))
   test<-nrow(df_pdb)
+  w<-0
   for (p in 1:nrow(df_complex)) {
-    if(file.exists(paste0("interaction/",df_complex$receptor_ligand[p],"/",df_complex$new_number[p],".csv"))){
-      df_protein<-read.csv(paste0("interaction/",df_complex$receptor_ligand[p],"/",df_complex$new_number[p],".csv"),
+    a<-strsplit(df_complex$models.y[p],split = ".",fixed = T)[[1]][1]
+    b<-strsplit(a,split = "_",fixed = T)[[1]][2]
+    if(file.exists(paste0("interaction/",df_complex$receptor_ligand[p],"/",b,".csv"))){
+      df_protein<-read.csv(paste0("interaction/",df_complex$receptor_ligand[p],"/",b,".csv"),
                            stringsAsFactors = F) 
       df_pdb$number_interactions[df_pdb$resno%in%df_protein$resid]<-df_pdb$number_interactions[df_pdb$resno%in%df_protein$resid]+1
       df_pdb$tested_structure<-df_pdb$tested_structure+1
       
+    }else{
+      w<-w+1
+      print(paste0("interaction/",df_complex$receptor_ligand[p],"/",b,".csv"))
     }
   }
-  df_pdb<-df_pdb%>%filter(tested_structure==total_structure)
-  if(nrow(df_pdb)==test){
-    df_pdb<-df_pdb%>%select(resno,resid,x,y,z,number_interactions,tested_structure,total_structure)
-    df_pdb<-df_pdb%>%mutate(persent_interactions=number_interactions/total_structure*100)
-    write.csv(df_pdb,
-              paste0("interaction_serf/",v_structure[j],".csv"),row.names = F)
-  }
+  #  df_pdb<-df_pdb%>%filter(tested_structure==total_structure)
+  #  if(nrow(df_pdb)==test){
+  #    df_pdb<-df_pdb%>%select(resno,resid,x,y,z,number_interactions,tested_structure,total_structure)
+  #    df_pdb<-df_pdb%>%mutate(persent_interactions=number_interactions/total_structure*100)
+  df_pdb<-df_pdb%>%mutate(persent_interactions=number_interactions/tested_structure*100)
+  write.csv(df_pdb,
+            paste0("interaction_serf/",v_structure[j],".csv"),row.names = F)
+  #  }else {
+  #    print(paste0(i," ",v_structure[j]," ",w))
+  #  }
 }
+
 df_all<-df_all%>%mutate(receptor_ligand=paste0(receptor,"_",ligand))
 df_all<-df_all%>%select(name.x,receptor,ligand,receptor_ligand)
 df_all<-unique(df_all)
@@ -72,7 +82,7 @@ v_seq<-seq(from=0,to =max(df_topology$seq_end),by=10)
 p<-ggplot()+
   geom_rect(aes(xmin = seq_beg-0.5, xmax = seq_end+0.5, ymin = -Inf, ymax = Inf,fill=topology,alpha=0.1),data=df_topology)+
   geom_point(aes(x=resno,y=number),data=df_pdb)+
-
+  
   theme_bw()+facet_grid(receptor_ligand~., scales = "free")+
   scale_x_continuous(breaks = v_seq,labels = v_seq)+
   guides(alpha = "none")
