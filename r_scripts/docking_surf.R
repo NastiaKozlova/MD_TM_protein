2142f925edd3697d9d55f25bbbb4eb4e2ee9d4dc
NastiaKozlova <kozlovaas.93gmail.com>
2026-05-29 10:38:17 -0500

    fix script

--- r_scripts/docking_surf.R
+++ r_scripts/docking_surf.R
@@ -1,10 +1,12 @@
-part_name <- commandArgs(trailingOnly=TRUE)
+part <- commandArgs(trailingOnly=TRUE)
+part_TEMP<-strsplit(part,split = ",",fixed = T)[[1]]
+part_start<-part_TEMP[1]
+v_search<-part_TEMP[2]
 library(ggplot2)
 library(bio3d)
 library(dplyr)
-setwd(part_start)
-
-#part_name<-paste0(part_start,"MD_analysis/docking/docking_first/")
+#setwd(part_start)
+part_name<-paste0(part_start,"MD_analysis/docking/docking_first/",v_search,"/")
 
 #system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/docking/docking_group_structure.R ",part_name,",1"),ignore.stdout=T,wait = T)
 
